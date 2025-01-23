7b36e37fbb2d5880aea214b3ca9ba86b32c596fd
gr0vity-dev <85646666+gr0vity-dev@users.noreply.github.com>
2025-01-23 09:48:39 +0100

    make docker gateway discovery more robust on linux

--- nanomock/docker/linux.py
+++ nanomock/docker/linux.py
@@ -7,6 +7,37 @@ class LinuxDockerInterface(DockerMixin, DockerInterface):
 
     @classmethod
     def get_docker_gateway_ip(cls) -> str:
-        command = "ifconfig | grep -A1 'docker' | grep inet | awk '{print $2}'"
-        output = subprocess.check_output(command, shell=True, text=True)
-        return output.strip()
+        # Try ip address first
+        ip_cmd = (
+            "ip address show docker0 | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1"
+        )
+        try:
+            output = subprocess.check_output(ip_cmd, shell=True, text=True).strip()
+            if output:
+                return output
+        except subprocess.CalledProcessError:
+            pass
+
+        # Fallback to ifconfig if available
+        ifconfig_cmd = "ifconfig docker0 2>/dev/null | grep 'inet ' | awk '{print $2}'"
+        try:
+            output = subprocess.check_output(
+                ifconfig_cmd, shell=True, text=True
+            ).strip()
+            if output:
+                return output
+        except subprocess.CalledProcessError:
+            pass
+
+        # Last resort - try docker network inspect
+        docker_cmd = (
+            "docker network inspect bridge | grep Gateway | awk -F'\"' '{print $4}'"
+        )
+        try:
+            output = subprocess.check_output(docker_cmd, shell=True, text=True).strip()
+            if output:
+                return output
+        except subprocess.CalledProcessError:
+            pass
+
+        raise RuntimeError("Could not determine Docker gateway IP")
--- setup.py
+++ setup.py
@@ -3,27 +3,29 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name="nanomock",
-      version="0.0.24",
-      author="gr0vity",
-      description="Create local dockerized nano-currency networks",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="https://github.com/gr0vity-dev/nanomock",
-      packages=find_packages(exclude=["unit_tests"]),
-      include_package_data=True,
-      install_requires=[
-          "pyyaml",
-          "tomli_w",
-          "tomli",
-          "oyaml",
-          "nano_lib_py",
-          "extradict",
-          "nanorpc",
-          'bitmath',
-      ],
-      entry_points={
-          'console_scripts': [
-              'nanomock=nanomock.main:main',
-          ],
-      })
+setup(
+    name="nanomock",
+    version="0.0.25",
+    author="gr0vity",
+    description="Create local dockerized nano-currency networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gr0vity-dev/nanomock",
+    packages=find_packages(exclude=["unit_tests"]),
+    include_package_data=True,
+    install_requires=[
+        "pyyaml",
+        "tomli_w",
+        "tomli",
+        "oyaml",
+        "nano_lib_py",
+        "extradict",
+        "nanorpc",
+        "bitmath",
+    ],
+    entry_points={
+        "console_scripts": [
+            "nanomock=nanomock.main:main",
+        ],
+    },
+)
